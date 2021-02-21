451e4817c3b1ccc347c6677915ebcbf916aa9e87
Feng Jiayi <mua6001@163.com>
2021-02-20 22:57:13 -0500

    modification page

--- themes/theme-4w4/functions.php
+++ themes/theme-4w4/functions.php
@@ -142,7 +142,7 @@ add_action( 'widgets_init', 'theme_4w4_widgets_init' );
 function theme_4w4_scripts() {
 	wp_enqueue_style( 'theme-4w4-style', get_stylesheet_uri(), array(), _S_VERSION );
 
-	wp_enqueue_style( 'theme-4w4-google-fonts', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap', false );
+	wp_enqueue_style( 'theme-4w4-google-fonts', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap', false);
 
 	wp_style_add_data( 'theme-4w4-style', 'rtl', 'replace' );
 
