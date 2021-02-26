bec0f1625342439ab26759ca5410088f5df624c8
Feng Jiayi <mua6001@163.com>
2021-02-26 11:28:16 -0500

    readme.md

--- themes/theme-4w4/header.php
+++ themes/theme-4w4/header.php
@@ -28,6 +28,7 @@
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
+		
 			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
 				?>
@@ -43,7 +44,24 @@
 				?>
 				<p class="site-description"><?php echo $theme_4w4_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 			<?php endif; ?>
-		</div><!-- .site-branding -->
+		</div>
+		<!-- .site-branding -->
+	
+		<!-- carrousel -->
+		<?php 
+		if(is_front_page()): ?>
+				<section class="carrousel">
+				<div>1</div>
+				<div>2</div>
+				<div>3</div>
+				</section>
+
+				<button id="un">1</button>
+				<button id="deux">2</button>
+				<button id="trois">3</button>
+				
+		<?php endif?>
+			
 
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><img src="https://s2.svgbox.net/hero-solid.svg?ic=menu&color=000000" width="32" height="32"></button>
