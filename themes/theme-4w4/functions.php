d73ee0d28f877528c942d25dce51bccb2d4a2a96
Feng Jiayi <mua6001@163.com>
2021-03-18 22:27:46 -0400

    add script dans fonction php

--- themes/theme-4w4/functions.php
+++ themes/theme-4w4/functions.php
@@ -194,6 +194,16 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+function extraire_cours($query){
+	if($query->is_category('cours'))
+	{
+	$query->set( 'category_name', 'cours' );
+	$query->set('orderby', 'title');
+	$query->set('order', 'asc');
+	}
+}
+add_action('pre_get_posts','extraire_cours');
+
 function extraire_cours_front_page($query){
 	if( !is_admin() && $query->is_front_page() && $query->is_main_query() ){
 
