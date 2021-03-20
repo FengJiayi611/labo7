d53c6e54392c254401a5a25533bb593c67a489a4
Feng Jiayi <mua6001@163.com>
2021-03-20 17:18:07 -0400

    modification pour front-page.php (correction erreur)

--- themes/theme-4w4/front-page.php
+++ themes/theme-4w4/front-page.php
@@ -10,6 +10,7 @@
 get_header();
 ?>
 ////front-page.php
+
 	<main id="primary" class="site-main">
 
 		<?php if ( have_posts() ) : ?>
@@ -20,30 +21,43 @@ get_header();
 				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
+			<section class="cours">
 
 			<?php
-			/* Start the Loop */
-            $precedent = 0;
+			/* Start the Loop */		
+            $precedent = "XXXXXX";
 			while ( have_posts() ) :
 				the_post();
                 $titre = get_the_title();
-                $session = substr($titre, 4, 1);
-                $contenu = get_the_content();
-                $resume = substr($contenu, 0, 200);
 
-                ?>
-		       <?php 
-                    if($session != $precedent){
-                        echo"<p>session : " . $session . "</p>";
-                    }
-                    $precedent = $session;
-               ?>
-               <P> <?php echo $session . " - " . $titre; ?> </P>
-               <p> <?php echo $resume; ?></p>
-            <?php
-			endwhile;    
-		endif;
-		?>
+                //582-3C1 Design d’interactivité (75h)
+				$sigle= substr($titre, 0, 7);
+				$nbHeure = substr($titre, -4, 3);
+				$titrePartiel = substr($titre, 8, -6);
+				$session = substr($titre, 4, 1);
+				
+                //$contenu = get_the_content();
+                //$resume = substr($contenu, 0, 200);
+				$typeCours = get_field( "type_de_cours" );
+				
+				if($typeCours != $precedent):
+					if ("XXXXXX" != $precedent) : ?>
+			    </section>
+				<?php endif?>
+				<h2><?php echo $typeCours ?></h2>
+				<section>
+				<?php endif?>
+
+				<article>
+				 <p><?php echo $sigle . " - " . $typeCours . " - " . $nbHeure ; ?></p>
+				 <a href="<?php echo get_permalink() ?>"><?php echo $titrePartiel; ?></a>
+				 <p>Session : <?php echo $session; ?></p>
+				</article>
+            <?php 
+		   $precedent = $typeCours;
+			endwhile; ?>  
+			</section> <!-- fin section cours--> 
+		<?php endif;?>
 
 	</main><!-- #main -->
 
